9117f4f9e419cec86766137a133c4343f5721576
ranje <ranje@LAPTOP-4IAQGGJI>
2018-08-08 13:42:26 +0800

    Refactored code by adding @PostConstruct annotation to load data earlier

--- jackson-databind-json-demo/src/main/java/com/ranjen/springmvcrest/rest/StudentRestController.java
+++ jackson-databind-json-demo/src/main/java/com/ranjen/springmvcrest/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.ranjen.springmvcrest.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +14,25 @@ import com.ranjen.springmvcrest.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	// define @PostConstruct to load the student data ... only once!
+	@PostConstruct
+	public void loadData() {
+	
+		theStudents = new ArrayList<>();
+		
+		theStudents.add(new Student("Poornima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));		
+	}
 
 	// define endpoint for "/students" - return list of students
 	
 	@GetMapping("/students")
 	public List<Student> getStudents() {
-
-		List<Student> theStudents = new ArrayList<>();
-		
-		theStudents.add(new Student("Poornima", "Patel"));
-		theStudents.add(new Student("Mario", "Rossi"));
-		theStudents.add(new Student("Mary", "Smith"));		
-			
+		//now theStudents already loaded with the @PostConstruct data 	
 		return theStudents;
 	}
 	
