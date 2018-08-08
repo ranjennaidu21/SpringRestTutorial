5750f08bc5bb6a3cdf6d13cd80e080774fa53860
ranje <ranje@LAPTOP-4IAQGGJI>
2018-08-08 13:54:08 +0800

    Path Variable for REST Endpoints to get student by passing id

--- jackson-databind-json-demo/src/main/java/com/ranjen/springmvcrest/rest/StudentRestController.java
+++ jackson-databind-json-demo/src/main/java/com/ranjen/springmvcrest/rest/StudentRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,21 @@ public class StudentRestController {
 	public List<Student> getStudents() {
 		//now theStudents already loaded with the @PostConstruct data 	
 		return theStudents;
+	}
+	
+	// define endpoint for "/students/{studentId}" - return student at index
+	//will display the student information based on the id entered in the url
+	@GetMapping("/students/{studentId}")
+	//by default @PathVariable variable name should be matched with above {studentId}
+	//for this there is problem if user entered wrong index number , so need to catch
+	//the exception for that.
+	public Student getStudent(@PathVariable int studentId) {
+		
+		// just index into the list ... keep it simple for now
+				
+		//get the element of the list based on index using get method in List
+		return theStudents.get(studentId);
+		
 	}
 	
 }
